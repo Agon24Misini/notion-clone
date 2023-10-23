64872e231175a652fb95023293c5258e6a05b7ee
Agon24Misini <agonmisini26@gmail.com>
2023-10-23 18:48:38 +0200

    Sidebar

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -27,7 +27,7 @@ const Heading = () => {
       )}
       {isAuthenticated && !isLoading && (
         <Button asChild>
-          <Link href='/docunents'>
+          <Link href='/documents'>
             Enter Jotion
             <ArrowRight className='h-4 w-4 ml-2' />
           </Link>
