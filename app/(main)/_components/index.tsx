22d31df26a57802bcffa2be6686129ac56e809f4
Agon24Misini <agonmisini26@gmail.com>
2023-10-26 12:37:01 +0200

    Document list

--- app/(main)/_components/index.tsx
+++ app/(main)/_components/index.tsx
@@ -1,5 +1,6 @@
 import Navigation from "./navigation";
 import UserItem from "./user-item";
 import Item from "./item";
+import DocumentList from "./document-list";
 
-export { Navigation, UserItem, Item };
+export { Navigation, UserItem, Item, DocumentList };
